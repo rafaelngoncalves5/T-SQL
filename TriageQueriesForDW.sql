0a1331464e95b41daeaa499fceb9c3a757cd6843
Admin <rafael.n.goncalves@avanade.com>
2026-08-17 19:08:37 -0300

    Performance queries

--- TriageQueriesForDW.sql
+++ TriageQueriesForDW.sql
@@ -24,3 +24,31 @@ FROM queryinsights.sql_pool_insights;
 SELECT TOP 5 *
 FROM queryinsights.long_running_queries
 ORDER BY last_run_total_elapsed_time_ms DESC;
+
+-- 6) View my permissions
+SELECT *
+FROM fn_my_permissions(NULL, 'Database'); -- Second parameter = scope
+
+-- 7) New metadata sync: Once activated - you can query to check status of last syncing
+SELECT *
+FROM sys.dm_db_external_tables_log_status;
+
+-- 8) DMVs to identify and kill long-running queries
+SELECT *
+FROM sys.dm_exec_requests
+WHERE status = 'running'
+ORDER BY total_elapsed_time DESC;
+
+-- 8.1) You can then grab the session and kill them
+KILL 142;
+
+-- 9) Top 10 expensive queries (queryinsights not DMVs - prefix queryinsights, not sys)
+SELECT TOP 10 *
+FROM queryinsights.exec_requests_history
+ORDER BY allocated_cpu_time_ms DESC;
+
+-- 9.1) Top 10 most expensive within last 24 hours
+SELECT TOP 10 *
+FROM queryinsights.exec_requests_history
+WHERE submit_time >= DATEADD(DAY, -1, GETUTCDATE())
+ORDER BY allocated_cpu_time_ms DESC;
